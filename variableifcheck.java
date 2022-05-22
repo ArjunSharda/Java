5debd14f6a87808a29e8ae24fec216cc383eb43b
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-21 21:14:44 -0500

    Create variableifcheck.java

---
+++ variableifcheck.java
@@ -0,0 +1,16 @@
+// Credit to https://www.w3schools.com/java/java_variables.asp for help with this
+class Main {
+  public static void main(String[] args) {
+    int test1 = 29;
+    int test2 = 29;
+    if (test1 > test2) {
+      System.out.println("Test1 is greater than test2!");  
+      }
+    if (test2 > test1) {
+      System.out.println("Test2 is greater than test1!");
+    }
+    if (test1 == test2) {
+      System.out.println("Test1 and test2 are equal!");
+    }
+  }
+}
