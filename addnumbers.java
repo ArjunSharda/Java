7076039d0ba6b4e180735f16da6efb0e36468f71
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-21 20:26:26 -0500

    Create addnumbers.java

---
+++ addnumbers.java
@@ -0,0 +1,8 @@
+// Credit to https://www.w3schools.com/java/java_variables.asp for help with this
+class Main {
+  public static void Main(String[] args) {
+    int test1 = 10;
+    int test2 = 20;
+    System.out.println(test1+test2);
+  }
+}
