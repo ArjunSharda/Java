01dd277ab35845a6b9db1824bba32a113cebc222
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-21 20:50:36 -0500

    Create multiplynumbers.java

---
+++ multiplynumbers.java
@@ -0,0 +1,8 @@
+// Credit to https://www.w3schools.com/java/java_variables.asp for help with this
+class Main {
+  public static void main(String[] args) {
+    int test1 = 30;
+    int test2 = 10;
+    System.out.println(test1*test2);
+  }
+}
