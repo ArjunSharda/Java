ae5db31221f928e5a2a523e1e83716ca6d411d69
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-22 10:11:20 -0500

    Create methodsquare.java

---
+++ methodsquare.java
@@ -0,0 +1,11 @@
+// Credit to https://www.w3schools.com/java/java_variables.asp for help with this
+
+public class Main {
+  static int square(int num1) {
+    return num1 * num1;
+  }
+
+public static void main(String[] args) {
+  System.out.println(square(6));
+}
+}
