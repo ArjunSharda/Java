76986e4941f43df111bd3c0aaf7c7a4cf7d6ec4b
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-22 10:09:16 -0500

    Create methodmultiplication.java

---
+++ methodmultiplication.java
@@ -0,0 +1,11 @@
+// Credit to https://www.w3schools.com/java/java_variables.asp for help with this
+
+public class Main {
+  static int multiplication(int num1, int num2) {
+    return num1 * num2;
+  }
+
+public static void main(String[] args) {
+  System.out.println(multiplication(10, 10));
+}
+}
