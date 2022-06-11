f123e47c711fdfd3c64d600161afa1c71d61e864
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-11 14:18:35 -0500

    Create exponent.java

---
+++ exponent.java
@@ -0,0 +1,7 @@
+// Credit to https://www.w3schools.com/java/java_variables.asp for help with this
+class main {
+  public static void main(String[] args) {
+    int test1 = 6;
+    System.out.println(test1*test1);
+  }
+}
